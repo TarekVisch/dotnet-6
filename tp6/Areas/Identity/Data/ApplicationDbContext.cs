3eca47a8376c046029c3a54ce0ee52d9c966ba53
TarekVisch <tarek.chentouf.99@gmail.com>
2024-01-07 17:27:23 +0100

    Add project files.

---
+++ tp6/Areas/Identity/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using tp6.Areas.Identity.Data;
+using tp6.Models;
+
+namespace tp6.Data;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+
+    public DbSet<tp6.Models.ProductModel>? ProductModel { get; set; }
+}
---
+++ tp6/Models/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Linq;
+
+namespace tp6.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string description { get; set; } = "";
+        public float prix { get; set; }
+
+        public ProductModel()
+        {
+        }
+    }
+}
